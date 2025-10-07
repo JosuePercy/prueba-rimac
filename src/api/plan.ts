0fc54d7a380b7ebd82949ca6097459738661adf9
JosuePercy <josuehuallullo12@gmail.com>
2025-10-07 08:55:05 -0500

    refactor: Replace environment variable references with hardcoded API URLs in plan and user API modules

--- src/api/plan.ts
+++ src/api/plan.ts
@@ -2,7 +2,7 @@ import type { PlanResponse } from '../types/plan';
 import axios from 'axios';
 
 
-const API_PLANS = import.meta.env.VITE_API_PLANS; 
+const API_PLANS = "https://rimac-front-end-challenge.netlify.app/api/plans.json"
 
 export async function getPlans(): Promise<PlanResponse> {
   const { data } = await axios.get<PlanResponse>(API_PLANS);
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { User } from '../types/user';
 
-const API_USER = import.meta.env.VITE_API_USER; 
+const API_USER = "https://rimac-front-end-challenge.netlify.app/api/user.json";
 
 export async function getUser(): Promise<User> {
   const { data } = await axios.get<User>(API_USER);
